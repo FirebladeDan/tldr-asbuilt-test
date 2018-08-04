43af977ea36f4530a4d008ca77e6469f991cf681
dlozanoTest <d_lozano08@yahoo.com>
2018-08-04 14:14:59 -0500

    Router Init

---
+++ asbuilt/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavbarFeature from './NavbarFeature';
+import Router from './Router';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavbarFeature />
+        <Router />
+      </div>
+    );
+  }
+}
+
+export default App;
