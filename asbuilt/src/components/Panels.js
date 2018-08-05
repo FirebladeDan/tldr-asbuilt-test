ef4fccf095ba499bac771fb56e208e594a57987b
Trent Derr <t.l.derr@gmail.com>
2018-08-05 10:07:01 -0500

    Added clickedDI action function

--- asbuilt/src/components/Panels.js
+++ asbuilt/src/components/Panels.js
@@ -7,19 +7,26 @@ import './Panels.css';
 export class DIPanel extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {diName: ''};
     }
 
-  render() {
-    return (
-        <div className='DIPanel'>
-            <strong>Project Devices</strong>
-            <ul>
-                {this.props.diList.map( 
-                    (di)=><li onClick={this.handleClick}>
-                        {di.model}, {di.id}
-                </li>)}
-            </ul>
-        </div>
-    )
-  }
+    clickedDI(id, e) {
+        e.preventDefault();
+        this.setState(diName => ({diName: id}));
+    }
+
+    render() {
+        return (
+            <div className='DIPanel'>
+                <strong>Project Devices</strong>
+                <ul>
+                    {this.props.diList.map( 
+                        (di)=><li onClick={(e) => this.clickedDI(di.id, e)}>
+                            {di.model}, {di.id}
+                    </li>)}
+                </ul>
+                <h1>{diName}</h1>
+            </div> 
+        );
+    }
 }
